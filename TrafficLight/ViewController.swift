3cdc53eceb88481bfb5fb5cd11d2ad7862631770
Vladislav Starikov <yurqqqaa@icloud.com>
2023-09-25 20:20:01 +0500

    My 2 HomeWork

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redColorLight: UIView!
+    @IBOutlet weak var yellowColorLight: UIView!
+    @IBOutlet weak var greenColorLight: UIView!
+    
+    @IBOutlet weak var turnButton: UIButton!
+    
     override func viewDidLoad() {
+        redColorLight.layer.cornerRadius = 100
+        yellowColorLight.layer.cornerRadius = 100
+        greenColorLight.layer.cornerRadius = 100
+        
+        redColorLight.alpha = 0.5
+        yellowColorLight.alpha = 0.5
+        greenColorLight.alpha = 0.5
+        
+        turnButton.layer.cornerRadius = 10
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func activationButton() {
+        if redColorLight.alpha == 0.5 && yellowColorLight.alpha == 0.5 && greenColorLight.alpha == 0.5  { //clear
+            redColorLight.alpha = 1    // red
+        } else if redColorLight.alpha == 1 { //red
+            redColorLight.alpha = 0.5 // clear
+            yellowColorLight.alpha = 1 // yellow
+        } else if yellowColorLight.alpha == 1 { //yellow
+            yellowColorLight.alpha = 0.5  //clear
+            greenColorLight.alpha = 1  //green
+        } else if greenColorLight.alpha == 1 {
+            greenColorLight.alpha = 0.5
+        }
+    }
 }
 
